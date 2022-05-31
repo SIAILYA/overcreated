6942b4050b46b9544ce63324ca8649c4c67b6a69
siailya <sia.ilya.dev@gmail.com>
2022-05-31 14:32:37 +0300

    Сделал страницу логина на клиенте

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,13 +2,15 @@ import Portfolio from "@/views/Portfolio.vue";
 import Resume from "@/views/Resume.vue";
 import Timeline from "@/views/Timeline.vue";
 import Contacts from "@/views/Contacts.vue";
+import Login from "@/views/Login.vue";
 import {createRouter, createWebHistory} from "vue-router"
 
 const routes = [
     {path: "/", component: Portfolio},
     {path: "/resume", component: Resume},
     {path: "/contacts", component: Contacts},
-    {path: "/timeline", component: Timeline}
+    {path: "/timeline", component: Timeline},
+    {path: "/login", component: Login}
 ]
 
 const router = createRouter({
