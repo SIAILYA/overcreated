b2bc7f4a9f0bcfeaf826b371fae6717002d43bf2
siailya <sia.ilya.dev@gmail.com>
2022-06-01 09:47:36 +0300

    Сделал компонент спиннера и добавил его в фетчер проектов

---
+++ client/src/components/OvcSpinner.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="spinner__wrapper" :class="!show ? 'hide' : ''">
+    <span class="spinner">
+    </span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  show: Boolean
+})
+</script>
+
+<style scoped>
+.spinner__wrapper {
+  width: 50px;
+  height: 50px;
+  overflow: hidden;
+  transition: all .3s ease;
+}
+
+.spinner__wrapper.hide {
+  height: 0;
+  width: 0;
+}
+
+.spinner {
+  background: var(--accent);
+  color: var(--accent);
+  animation: spinner 1s ease infinite;
+  transition: all .3s ease;
+  display: block;
+  width: 100%;
+  height: 100%;
+  border-radius: 1000px;
+}
+
+@keyframes spinner {
+  from {
+    transform: scale(0);
+    opacity: 0;
+  }
+  to {
+    transform: scale(1);
+    opacity: .8;
+  }
+}
+</style>
--- client/src/views/admin/projects/Projects.vue
+++ client/src/views/admin/projects/Projects.vue
@@ -5,6 +5,10 @@
       <span>Создать проект</span>
     </button>
   </router-link>
+  <br>
+  <div class="d-flex mt-3">
+    <ovc-spinner class="mx-auto" :show="isLoadingProjects"/>
+  </div>
   <div class="row mt-5">
     <div v-for="p in projects" :key="p.id" class="col-12 col-md-6 col-lg-4 d-flex">
       <ovc-project :project="p" :to="'edit/' + p.id" class="project-card"/>
@@ -18,14 +22,18 @@ import {onMounted, Ref, ref} from "vue";
 import axios from "axios";
 import {BACK_API} from "../../../../config";
 import OvcProject from "../../../components/OvcProject.vue";
+import OvcSpinner from "../../../components/OvcSpinner.vue";
 
 const projects: Ref<Array<Project>> = ref([])
+const isLoadingProjects = ref(true)
 
 onMounted(() => {
   axios.get(BACK_API + "projects/get?onlyVisible=false").then(r => {
     projects.value = r.data.map((p: Object) => {
       return new Project(p);
     })
+
+    isLoadingProjects.value = false
   })
 })
 </script>
