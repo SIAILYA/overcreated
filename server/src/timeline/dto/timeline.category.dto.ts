cf314eb7894541a39c2be00a67231f1a1808fc3e
siailya <sia.ilya.dev@gmail.com>
2022-06-02 21:10:22 +0300

    Добавил серверную логику для таймлайна и его категорий

---
+++ server/src/timeline/dto/timeline.category.dto.ts
@@ -0,0 +1,6 @@
+import {TimelineCategory} from "../schemas/timeline.category.schema";
+
+export class TimelineCategoryDto {
+  readonly title: string
+  readonly color: string
+}
---
+++ server/src/timeline/dto/timeline.event.dto.ts
@@ -0,0 +1,6 @@
+export class TimelineEventDto {
+  readonly title: string
+  readonly description: string
+  readonly date: Date
+  readonly topics: string
+}
