e98e63fde3441b06f47b61a17de68044722cace0
Ilya <sia.ilya.dev@gmail.com>
2023-01-21 15:59:36 +0300

    fix<server>: В projectTopics.controller был указан неверный сервис, для techs.service переопределен метод getAllVisible (т.к. у технологий нет поля isVisible)

--- server-v3/src/modules/portfolio/controllers/projectTopics.controller.ts
+++ server-v3/src/modules/portfolio/controllers/projectTopics.controller.ts
@@ -1,11 +1,11 @@
 import {Controller} from "@nestjs/common";
-import {ProjectsService} from "../services/projects.service";
 import {BaseReadController} from "../../../common/base/controller/baseRead.controller";
 import {ProjectTopicModel} from "../entities/projectTopic.model";
+import {ProjectTopicsService} from "../services/projectTopics.service";
 
 @Controller('api/v3/portfolio/projectTopics')
 export class ProjectTopicsController extends BaseReadController<ProjectTopicModel> {
-    constructor(private readonly projectsService: ProjectsService) {
+    constructor(private readonly projectsService: ProjectTopicsService) {
         super(projectsService)
     }
 }
--- server-v3/src/modules/portfolio/services/techs.service.ts
+++ server-v3/src/modules/portfolio/services/techs.service.ts
@@ -2,9 +2,14 @@ import {BaseService} from "../../../common/base/service/base.service";
 import {TechModel} from "../entities/tech.model";
 import {Repository} from "typeorm";
 import {InjectRepository} from "@nestjs/typeorm";
+import {GetAllParamsDto} from "../../../common/base/controller/dto/getAll.params.dto";
 
 export class TechsService extends BaseService<TechModel> {
     constructor(@InjectRepository(TechModel) protected readonly techModelRepository: Repository<TechModel>) {
         super(techModelRepository);
+    }
+
+    getAllVisible(getAllParams: GetAllParamsDto): Promise<Array<TechModel>> {
+        return this.getAll(getAllParams);
     }
 }
