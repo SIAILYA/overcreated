e533b4687a822dded394161e14fbf125181b4eb8
siailya <sia.ilya.2017@gmail.com>
2023-02-21 19:57:05 +0300

    fix<client>: Модалка при CSR отображается правильно

--- client/components/ovc-modal.vue
+++ client/components/ovc-modal.vue
@@ -29,6 +29,10 @@ const emit = defineEmits(['close'])
 .modal {
   position: fixed;
   z-index: 1000;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   display: flex;
   width: 100vw;
   height: 100vh;
