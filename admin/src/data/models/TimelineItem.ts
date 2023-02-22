e8b41b0d2edfa40338fac154f5e964118472e386
siailya <sia.ilya.2017@gmail.com>
2023-02-22 15:10:34 +0300

    feature<admin>: Поля модели точки таймлайна

--- admin/src/data/models/TimelineItem.ts
+++ admin/src/data/models/TimelineItem.ts
@@ -1,8 +1,29 @@
 import {ApiModel} from "@data/models/common/ApiModel";
 import {API} from "@data/models/common/API";
+import {Entity} from "@data/decorators/Entity";
+import {TimelineTopic} from "@data/models/TimelineTopic";
+import {Column} from "@data/decorators/Column";
 
+@Entity()
 export class TimelineItem extends ApiModel {
     api = new API('/timeline/timelineItems')
 
 
+    @Column({type: Date})
+    date!: Date
+
+    @Column()
+    readable_date!: string
+
+    @Column()
+    title!: string
+
+    @Column()
+    addition_description?: string
+
+    @Column()
+    description?: string
+
+    @Column({type: TimelineTopic})
+    topic!: TimelineTopic
 }
