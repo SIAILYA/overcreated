36eaf7a9a833992136723d7eb884350269fb6e4b
siailya <sia.ilya.2017@gmail.com>
2023-02-22 12:59:57 +0300

    fix<server>: Из топика таймлайна удален slug

--- server/src/modules/timeline/entities/timelineTopic.model.ts
+++ server/src/modules/timeline/entities/timelineTopic.model.ts
@@ -7,8 +7,6 @@ export class TimelineTopicModel extends BaseModel {
     @Column({type: 'varchar', length: 255, nullable: false})
     title!: string
 
-    @Column({type: 'varchar', length: 255, nullable: false})
-    slug!: string
 
     @Column({type: 'varchar', length: 7, nullable: false})
     color!: string
