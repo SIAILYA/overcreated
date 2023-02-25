a728a5421c5ef8a63e208f7bcbb26b21cee39566
siailya <sia.ilya.2017@gmail.com>
2023-02-25 11:26:01 +0300

    feature<client>: Добавлены секции с описаниями

--- client/pages/resume.vue
+++ client/pages/resume.vue
@@ -28,9 +28,12 @@
           <h4 class="font-light italic leading-tight">aka siailya</h4>
           <h3 class="text-2xl mt-5 lg:mt-8 mb-3">Я - веб-разработчик</h3>
 
-          <button
-              class="mt-auto text-white mr-auto py-2 px-4 rounded mb-7 inline-block transition-all download-btn duration-300"
-              style="background: var(--accent-purple);">
+          <nuxt-link
+              class="text-white mr-auto py-2 px-4 rounded mt-7 inline-block transition-all download-btn duration-300"
+              style="background: var(--accent-purple);"
+              to="https://disk.yandex.ru/i/WDM1jsSOXCQbtA"
+              target="_blank"
+          >
             <svg class="inline mr-1" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"
                  xmlns:xlink="http://www.w3.org/1999/xlink">
               <path
@@ -38,8 +41,54 @@
                   fill="currentColor"></path>
             </svg>
             Резюме в PDF
-          </button>
+          </nuxt-link>
         </div>
+      </div>
+      <div>
+        <h3 class="mt-3 text-2xl font-medium mb-3">Что я умею - мои <span style="color: var(--accent-purple)">hard-skills</span>
+        </h3>
+        <ul class="skills-list list-disc mb-0 pl-5">
+          <li> Мой основной ЯП - JS</li>
+          <li> Помимо JS хорошо знаю Python, работаю на Java(8+), не боюсь пробовать и что-то экзотическое</li>
+          <li> Умею работать с веб-фреймворками, понимаю, зачем они нужны. Люблю Vue 2/3, работал с React 16/17 и
+            Svelte
+          </li>
+          <li> Больше об используемых технологиях на странице
+            <ovc-emb-link color="var(--accent-purple)" to="/portfolio">портфолио</ovc-emb-link>
+          </li>
+          <li> На хорошем уровне знаю Python и сторонние библиотеки – от Pillow и Flask до NumPy и асинхронности</li>
+          <li> Понимаю принципы ООП, SOLID, знаю зачем они нужны и почему их используют</li>
+          <li> Работал с SQL и NoSQL. Люблю ORM, но пишу и самостоятельные запросы</li>
+          <li> Люблю контейнеризацию и CI/CD. Работал с Ansible, Jenkins, Swarm, GitHub Actions</li>
+          <li> Работаю на Windows, но дружу и с Linux. Чаще всего использую Ubuntu - и десктоп (если надо) и сервер</li>
+          <li> Имею начальные навыки UI/UX-дизайнера, умею работать в Figma, проектировать красивые и практичные
+            интерфейсы
+          </li>
+        </ul>
+
+        <hr class="my-6" style="border-color: var(--border-color)">
+
+        <h3 class="text-2xl font-medium mb-3">Какой я - мои <span style="color: var(--accent-purple)">soft-skills</span>
+        </h3>
+        <ul class="skills-list list-disc mb-0 pl-5">
+          <li> Люблю пробовать что-то новое в сфере IT, стараюсь оставаться в курсе последних новостей</li>
+          <li> Отлично владею русским языком – грамотно пишу, соблюдая грамматические и пунктуационные правила</li>
+          <li> Английским владею на среднем уровне – читаю почти любые тексты, понимаю четкую речь, могу поддержать
+            разговор на повседневные темы
+          </li>
+          <li> Умею быстро принимать решения и брать на себя ответственность</li>
+          <li> Легко приспосабливаюсь, подхожу к проблеме с разных сторон и находить креативные решения. Если что-то не
+            получается - остаюсь спокойным, не боюсь экспериментировать и просить помощи
+          </li>
+          <li> Легко могу одновременно держать в голове множество задач и работать над ними, всегда ищу способы
+            оптимизировать рабочй процесс
+          </li>
+          <li> Ориентирован на результат и достижение цели. А если задача объемная, могу разбить её на подзадачи и
+            работать над ними
+          </li>
+          <li> Когда требуется, могу поддержать разговор на множество тем – от серебрянного века до устройства ДВС</li>
+          <li> В любой сфере жизни руководствуюсь правилом «семь раз проверь – один раз отрежь»</li>
+        </ul>
       </div>
     </section>
   </main>
@@ -69,5 +118,13 @@ useHead({
 
 .download-btn:hover {
   box-shadow: 0 0 5px 3px rgba(223, 0, 106, .15);
+}
+
+.skills {
+  transition: all .3s ease;
+}
+
+.skills.collapsed {
+  height: 0 !important;
 }
 </style>
